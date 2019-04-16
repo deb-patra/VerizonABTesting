2e632d0a9bb5d8248ad7f9801de2520f9ecf213d
deb-patra <patra.deb@gmail.com>
2019-04-16 10:15:19 -0500

    war changes

--- src/main/java/com/incedo/VerizonAbTestingApplication.java
+++ src/main/java/com/incedo/VerizonAbTestingApplication.java
@@ -2,12 +2,17 @@ package com.incedo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class VerizonAbTestingApplication {
+public class VerizonAbTestingApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(VerizonAbTestingApplication.class, args);
 	}
-
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(VerizonAbTestingApplication.class);
+    }
 }
